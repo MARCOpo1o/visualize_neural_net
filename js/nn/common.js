af4584ca3dac6aa2c7a2c7cb3546ff655da965e2
Marco Qin <tqin@brandeis.edu>
2024-03-10 15:14:38 -0400

    changed

--- js/nn/common.js
+++ js/nn/common.js
@@ -88,79 +88,73 @@ function updateCubes() {
 function drawCubes() {
     var geometry = new THREE.Geometry();
     var pickingGeometry = new THREE.Geometry();
-    var pickingMaterial = new THREE.MeshBasicMaterial( { vertexColors: THREE.VertexColors } );
-    var defaultMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff, shading: THREE.FlatShading, vertexColors: THREE.VertexColors, transparent: true} );
+    var pickingMaterial = new THREE.MeshBasicMaterial({ vertexColors: THREE.VertexColors });
+    var defaultMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff, shading: THREE.FlatShading, vertexColors: THREE.VertexColors, transparent: true });
 
-    var geom = new THREE.BoxGeometry( 9,9,9 );
-    var hideGeom = new THREE.BoxGeometry(1,1,1);
-    var color = new THREE.Color();
-
+    var geom = new THREE.BoxGeometry(9, 9, 9);
+    var edgeGeom, edges; // Variables for edges geometry and edges themselves
+    var lineMaterial = new THREE.LineBasicMaterial({ color: 0x000000 }); // Material for the edges (black)
     var matrix = new THREE.Matrix4();
     var quaternion = new THREE.Quaternion();
+    var color = new THREE.Color();
 
-    for ( var i = 0; i < nNodes; i ++ ) {
+    for (var i = 0; i < nNodes; i++) {
+        var position = new THREE.Vector3();
+        position.x = posX[i];
+        position.y = posY[i];
+        position.z = posZ[i];
 
-	var position = new THREE.Vector3();
-	position.x = posX[i];
-	position.y = posY[i];
-	position.z = posZ[i];
+        var rotation = new THREE.Euler();
+        rotation.x = 0;
+        rotation.y = 0;
+        rotation.z = 0;
 
-	var rotation = new THREE.Euler();
-	rotation.x = 0;
-	rotation.y = 0;
-	rotation.z = 0;
+        var scale = new THREE.Vector3();
+        scale.x = 1;
+        scale.y = 1;
+        scale.z = 1;
 
-	var scale = new THREE.Vector3();
-	scale.x = 1;
-	scale.y = 1;
-	scale.z = 1;
-	
-	quaternion.setFromEuler( rotation, false );
-	matrix.compose( position, quaternion, scale );
-	
-	if (isComputed){
-	    var v = allNodeOutputs[i];
-	    var colorNum = math.round(v*99);
-	    r = redLookup[colorNum];
-	    g = greenLookup[colorNum];
-	    b = blueLookup[colorNum];
-	    applyVertexColors( geom, color.setRGB( r,g,b ) );
-	} else {
-	    applyVertexColors( geom, color.setRGB( 0,0,0 ) );
-	}
-	
-	geometry.merge( geom, matrix );
+        quaternion.setFromEuler(rotation, false);
+        matrix.compose(position, quaternion, scale);
 
-	// Create edges geometry and line material
-	var edges = new THREE.Geometry();
-	var lineMaterial = new THREE.LineBasicMaterial({ color: 0x000000 }); // Black color
+        if (isComputed) {
+            var v = allNodeOutputs[i];
+            var colorNum = Math.round(v * 99);
+            var r = redLookup[colorNum];
+            var g = greenLookup[colorNum];
+            var b = blueLookup[colorNum];
+            applyVertexColors(geom, color.setRGB(r, g, b));
+        } else {
+            applyVertexColors(geom, color.setRGB(0, 0, 0));
+        }
 
-	// Create line segments and add them to the scene
-	var lineSegments = new THREE.LineSegments(edges, lineMaterial);
-	lineSegments.applyMatrix(matrix);
-	scene.add(lineSegments);
+        geometry.merge(geom, matrix);
 
-	// give the geom's vertices a color corresponding to the "id"
+        // Create edges for the cube and merge them into the scene
+        edgeGeom = new THREE.EdgesGeometry(geom); // Create an edges geometry from the cube
+        edges = new THREE.LineSegments(edgeGeom, lineMaterial); // Create line segments to draw the edges
+        edges.applyMatrix4(matrix); // Apply the same transformation to the edges
+        scene.add(edges); // Add the edges to the scene
 
-	applyVertexColors( geom, color.setHex( i ) );
+        applyVertexColors(geom, color.setHex(i));
 
-	pickingGeometry.merge( geom, matrix );
+        pickingGeometry.merge(geom, matrix);
 
-	pickingData[ i ] = {
-	    position: position,
-	    rotation: rotation,
-	    scale: scale, 
-	    id: i
-	};
-
+        pickingData[i] = {
+            position: position,
+            rotation: rotation,
+            scale: scale,
+            id: i
+        };
     }
 
-    var drawnObject = new THREE.Mesh( geometry, defaultMaterial);
+    var drawnObject = new THREE.Mesh(geometry, defaultMaterial);
     drawnObject.name = 'cubes';
-    scene.add( drawnObject );
+    scene.add(drawnObject);
 
-    pickingScene.add( new THREE.Mesh( pickingGeometry, pickingMaterial ) );			
+    pickingScene.add(new THREE.Mesh(pickingGeometry, pickingMaterial));
 }
+
 function drawEdges() {
     //console.log('draw edges');
     var lineMat = new THREE.LineBasicMaterial({
